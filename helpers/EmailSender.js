86a7639444c98f46d721625d9db8cbbc9cf3e126
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-12-15 03:49:05 +0200

    meta

--- helpers/EmailSender.js
+++ helpers/EmailSender.js
@@ -1,20 +1,20 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-// const { META_PASS, META_MAIL, BASE_LOCAL_URL } = process.env;
-const { GMAIL_MAIL, GMAIL_PASS, BASE_RENDER_URL, BASE_LOCAL_URL } = process.env;
+const { META_PASS, META_MAIL, BASE_LOCAL_URL } = process.env;
+// const { GMAIL_MAIL, GMAIL_PASS, BASE_RENDER_URL, BASE_LOCAL_URL } = process.env;
 
 const mailerConfig = {
-    pool: true,
-    host: "smtp.gmail.com",
-    // host: "smtp.meta.ua",
+    // pool: true,
+    // host: "smtp.gmail.com",
+    host: "smtp.meta.ua",
     // host: "smtp.mail.yahoo.com",
     port: 465,
     secure: true,
-    requireTLS: true,
+    // requireTLS: true,
     auth: {
-        user: GMAIL_MAIL,
-        pass: GMAIL_PASS,
+        user: META_MAIL,
+        pass: META_PASS,
     },
     // tls: {
     //     // do not fail on invalid certs
@@ -27,9 +27,9 @@ const transport = nodemailer.createTransport(mailerConfig);
 async function EmailSender(mail, code) {
     const email = {
         to: mail,
-        from: GMAIL_MAIL,
+        from: META_MAIL,
         subject: "Verify your email please!",
-        html: `<a target="_blank" href="${BASE_RENDER_URL}/auth/verify/${code}">Click to verify your email</a>`,
+        html: `<a target="_blank" href="${BASE_LOCAL_URL}/auth/verify/${code}">Click to verify your email</a>`,
     };
 
     await transport.sendMail(email);
