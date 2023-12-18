0330c27ce99a9af4d7c25f5744db0a141411534f
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-12-18 22:20:47 +0200

    statistics

--- controllers/statsProtocol.js
+++ controllers/statsProtocol.js
@@ -9,7 +9,7 @@ const { Water, Weight, Food } = require("../models");
 
 async function getStatistics(req, res, next) {
     const { _id: owner } = req.user;
-    const { period, month } = req.query;
+    const { period, month, quantity } = req.query;
     const currentDate = new Date(Date.now());
     // const currentDate = LocaleDate();
 
@@ -47,6 +47,7 @@ async function getStatistics(req, res, next) {
                         { $match: { diary: "Dinner" } },
                         {
                             $group: {
+                                _id: null,
                                 protein: { $sum: "$protein" },
                                 fat: { $sum: "$fat" },
                                 carbohydrate: { $sum: "$carbohydrate" },
@@ -57,6 +58,7 @@ async function getStatistics(req, res, next) {
                         { $match: { diary: "Lunch" } },
                         {
                             $group: {
+                                _id: null,
                                 protein: { $sum: "$protein" },
                                 fat: { $sum: "$fat" },
                                 carbohydrate: { $sum: "$carbohydrate" },
@@ -67,6 +69,7 @@ async function getStatistics(req, res, next) {
                         { $match: { diary: "Snack" } },
                         {
                             $group: {
+                                _id: null,
                                 protein: { $sum: "$protein" },
                                 fat: { $sum: "$fat" },
                                 carbohydrate: { $sum: "$carbohydrate" },
@@ -88,62 +91,6 @@ async function getStatistics(req, res, next) {
     }
 
     if (period === "month" && month) {
-        // const beginDateMonth = new Date(Number(currentDate.getFullYear()), Number(currentDate.getMonth()), 1, 0);
-        // const endDateMonth = new Date(new Date(beginDateMonth).setMonth(new Date(beginDateMonth).getMonth() + 1));
-        // console.log(beginDateMonth);
-        // console.log(endDateMonth);
-
-        // const caloriesByDays = await Food.aggregate([
-        //     {
-        //         $match: {
-        //             owner,
-        //             date: {
-        //                 $gte: beginDateMonth.toISOString().split("T")[0],
-        //                 $lt: endDateMonth.toISOString().split("T")[0],
-        //             },
-        //         },
-        //     },
-        //     { $group: { _id: { $dateToString: { format: "%d", date: "$date" } }, amount: { $sum: "$total" } } },
-        //     { $sort: { _id: 1 } },
-        // ]).exec();
-
-        // const waterByDays = await Water.aggregate([
-        //     {
-        //         $match: {
-        //             owner,
-        //             date: {
-        //                 $gte: beginDateMonth.toISOString().split("T")[0],
-        //                 $lt: endDateMonth.toISOString().split("T")[0],
-        //             },
-        //         },
-        //     },
-        //     { $group: { _id: { $dateToString: { format: "%d", date: "$date" } }, amount: { $sum: "$water" } } },
-        //     { $sort: { _id: 1 } },
-        // ]).exec();
-
-        // const weightByDays = await Weight.aggregate([
-        //     {
-        //         $match: {
-        //             owner,
-        //             date: {
-        //                 $gte: beginDateMonth.toISOString().split("T")[0],
-        //                 $lt: endDateMonth.toISOString().split("T")[0],
-        //             },
-        //         },
-        //     },
-        //     { $group: { _id: { $dateToString: { format: "%d", date: "$date" } }, amount: { $sum: "$weight" } } },
-        //     { $sort: { _id: 1 } },
-        // ]).exec();
-
-        // res.json({
-        //     water: waterByDays,
-        //     weight: weightByDays,
-        //     calories: caloriesByDays,
-        // });
-
-        // const beginDateMonth = new Date(Number(currentDate.getFullYear()), Number(currentDate.getMonth()), 1, 3);
-        // const endDateMonth = new Date(new Date(beginDateMonth).setMonth(new Date(beginDateMonth).getMonth() + 1));
-
         const monthNumber = parseInt(month, 10);
 
         if (isNaN(monthNumber) || monthNumber < 1 || monthNumber > 12) {
@@ -170,13 +117,68 @@ async function getStatistics(req, res, next) {
 
         const waterByDays = await Water.aggregate([
             { $match: { owner, createdAt: { $gte: beginDateMonth, $lt: endDateMonth } } },
-            { $group: { _id: { $dateToString: { format: "%d", date: "$createdAt" } }, amount: { $sum: "$water" } } },
+            { $group: { _id: { $dateToString: { format: "%d", date: "$createdAt" } }, total: { $sum: "$water" } } },
             { $sort: { _id: 1 } },
         ]).exec();
 
         const weightByDays = await Weight.aggregate([
             { $match: { owner, createdAt: { $gte: beginDateMonth, $lt: endDateMonth } } },
-            { $group: { _id: { $dateToString: { format: "%d", date: "$createdAt" } }, amount: { $sum: "$weight" } } },
+            { $group: { _id: { $dateToString: { format: "%d", date: "$createdAt" } }, total: { $sum: "$weight" } } },
+            { $sort: { _id: 1 } },
+        ]).exec();
+
+        res.json({
+            water: waterByDays,
+            weight: weightByDays,
+            calories: caloriesByDays,
+        });
+
+        return;
+    }
+
+    if (period === "month" && quantity) {
+        const monthsNumber = parseInt(quantity, 10);
+
+        if (isNaN(monthsNumber) || monthsNumber < 1) {
+            return res.status(400).json({ message: "Invalid number of months. Please provide a positive integer." });
+        }
+
+        const beginDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - monthsNumber + 1, 1, 3);
+        const endDate = new Date(new Date(beginDate).setMonth(new Date(beginDate).getMonth() + monthsNumber));
+
+        const caloriesByDays = await Food.aggregate([
+            { $match: { owner, createdAt: { $gte: beginDate, $lt: endDate } } },
+            {
+                $group: {
+                    _id: { $dateToString: { format: "%Y-%m", date: "$createdAt" } },
+                    fat: { $sum: "$fat" },
+                    protein: { $sum: "$protein" },
+                    carbohydrate: { $sum: "$carbohydrate" },
+                    total: { $sum: "$total" },
+                },
+            },
+            { $sort: { _id: 1 } },
+        ]).exec();
+
+        const waterByDays = await Water.aggregate([
+            { $match: { owner, createdAt: { $gte: beginDate, $lt: endDate } } },
+            {
+                $group: {
+                    _id: { $dateToString: { format: "%Y-%m", date: "$createdAt" } },
+                    total: { $sum: "$water" },
+                },
+            },
+            { $sort: { _id: 1 } },
+        ]).exec();
+
+        const weightByDays = await Weight.aggregate([
+            { $match: { owner, createdAt: { $gte: beginDate, $lt: endDate } } },
+            {
+                $group: {
+                    _id: { $dateToString: { format: "%Y-%m", date: "$createdAt" } },
+                    total: { $sum: "$weight" },
+                },
+            },
             { $sort: { _id: 1 } },
         ]).exec();
 
@@ -193,64 +195,37 @@ async function getStatistics(req, res, next) {
         const beginDateYear = new Date(Number(currentDate.getFullYear() - 1), Number(currentDate.getMonth()), 1, 3);
         const endDateYear = new Date(new Date(currentDate));
 
-        console.log(beginDateYear);
-        console.log(endDateYear);
-
-        const caloriesByMonths = await Food.aggregate([
-            {
-                $match: {
-                    owner,
-                    date: {
-                        $gte: beginDateYear.toISOString().split("T")[0],
-                        $lt: endDateYear.toISOString().split("T")[0],
-                    },
-                },
-            },
-            {
-                $group: {
-                    _id: { $dateToString: { format: "%Y-%m", date: "$date" } },
-                    amount: { $sum: "$calories" },
-                    count: { $sum: 1 },
-                },
-            },
-            { $sort: { _id: 1 } },
-        ]).exec();
-
         const waterByMonths = await Water.aggregate([
-            {
-                $match: {
-                    owner,
-                    date: {
-                        $gte: beginDateYear.toISOString().split("T")[0],
-                        $lt: endDateYear.toISOString().split("T")[0],
-                    },
-                },
-            },
+            { $match: { owner, createdAt: { $gte: beginDateYear, $lt: endDateYear } } },
             {
                 $group: {
-                    _id: { $dateToString: { format: "%Y-%m", date: "$date" } },
-                    amount: { $sum: "$water" },
-                    count: { $sum: 1 },
+                    _id: { $dateToString: { format: "%Y-%m", date: "$createdAt" } },
+                    total: { $sum: "$water" },
                 },
             },
             { $sort: { _id: 1 } },
         ]).exec();
 
         const weightByMonths = await Weight.aggregate([
-            {
-                $match: {
-                    owner,
-                    date: {
-                        $gte: beginDateYear.toISOString().split("T")[0],
-                        $lt: endDateYear.toISOString().split("T")[0],
-                    },
-                },
-            },
+            { $match: { owner, createdAt: { $gte: beginDateYear, $lt: endDateYear } } },
             {
                 $group: {
-                    _id: { $dateToString: { format: "%Y-%m", date: "$date" } },
-                    amount: { $sum: "$weight" },
-                    count: { $sum: 1 },
+                    _id: { $dateToString: { format: "%Y-%m", date: "$createdAt" } },
+                    total: { $sum: "$weight" },
+                },
+            },
+            { $sort: { _id: 1 } },
+        ]).exec();
+
+        const caloriesByMonths = await Food.aggregate([
+            { $match: { owner, createdAt: { $gte: beginDateYear, $lt: endDateYear } } },
+            {
+                $group: {
+                    _id: { $dateToString: { format: "%Y-%m", date: "$createdAt" } },
+                    fat: { $sum: "$fat" },
+                    protein: { $sum: "$protein" },
+                    carbohydrate: { $sum: "$carbohydrate" },
+                    total: { $sum: "$total" },
                 },
             },
             { $sort: { _id: 1 } },
